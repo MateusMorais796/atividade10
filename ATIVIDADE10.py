9b4eff94f398579d24ae35ede96e96eeb4c808d2
MateusMorais796 <moraiscosta321@gmail.com>
2024-09-11 14:19:27 -0300

    Número é ímpar ou par

---
+++ ATIVIDADE10.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+numero = int(input("Digite o número "))
+
+
+if (numero % 2 == 0):
+    print("O número é par")
+else:
+    print("O número é ímpar")
